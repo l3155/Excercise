36cb97d0af9a94874969d878eff114b9ca5b4ab5
zjj <zhang5505@qq.com>
2018-01-27 15:12:51 +0800

    test

--- vue_demo/js/demo1.js
+++ vue_demo/js/demo1.js
@@ -1,9 +1,9 @@
-var app = new Vue({
+var app = new Vue ({
   el: '#app',
   data: {
     message: 'Hello Vue!'
   }
-})
+});
 
 var app2 = new Vue({
   el:'#app2',
@@ -15,6 +15,6 @@ var app2 = new Vue({
 var app3 = new Vue({
   el:'#app3',
   data:{
-    seen:true
+    seen:false
   }
 });
